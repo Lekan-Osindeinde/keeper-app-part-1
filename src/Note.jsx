c68d8bd2fd3e03915348f0fa50730868b7f2b390
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-09 16:57:54 +0100

    css update

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Note() {
-  return <div className='note'>
-  <h1>My Favourite Foods</h1>
-  <p>The Lord is so good to me and my family</p>
-  </div>;
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
-
