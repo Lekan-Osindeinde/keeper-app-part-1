77f0fd7eef8f29dd51facf17d341b4ebfc977017
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-11 01:40:49 +0100

    Updates

---
+++ precache-manifest.406a4061a85e08b66f4231ece5d69598.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca04e8a20935646745002844a68a6d3",
+    "url": "/keeper-app-part-1/index.html"
+  },
+  {
+    "revision": "abc719034a6d32a84136",
+    "url": "/keeper-app-part-1/static/js/2.7c83e95b.chunk.js"
+  },
+  {
+    "revision": "960965bb98e32f8afa44",
+    "url": "/keeper-app-part-1/static/js/main.3ae854b0.chunk.js"
+  },
+  {
+    "revision": "b70043f0fc0dd864c567",
+    "url": "/keeper-app-part-1/static/js/runtime-main.b2df22df.js"
+  }
+]);
